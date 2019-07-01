edb2d6cf3886a2b705ff9c338522469e4c0522ac
Richie <richieganney@gmail.com>
2019-07-01 16:05:58 +0100

    Maximum balance of £90 cannot be exceded

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -3,6 +3,7 @@ require 'pry'
 class Oystercard
 
   DEFAULT_BALANCE = 0
+  MAXIMUM_BALANCE = 90
 
   attr_reader :balance
 
@@ -11,8 +12,18 @@ class Oystercard
   end
 
   def top_up(amount)
-    amount += @balance
+    raise("Maximum limit of #{MAXIMUM_BALANCE} exceded") if @balance + amount > MAXIMUM_BALANCE
+    @balance += amount
+    puts "Topped up £#{amount}. New balance: £#{@balance}"
+    return @balance
   end
+
+  private
+
+  # def limit_exceded?
+  #   @balance > 90
+  # end
+
 end
 
-# binding.pry
+binding.pry
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -8,12 +8,22 @@ describe Oystercard do
 
   describe '#top_up' do
     it "Tops up the balance by a specified amount" do
-      expect(subject.top_up(5)).to eq 5
+      expect{ subject.top_up 5 }.to change{ subject.balance }.by 5
     end
 
     it "Tops up the balance by a specified amount" do
-      expect(subject.top_up(10)).to eq 10 
+      expect{ subject.top_up 10 }.to change{ subject.balance }.by 10
     end
+
+    it 'raises an error if maximum limit (£90) is reached' do
+      subject.top_up Oystercard::MAXIMUM_BALANCE
+      expect{ subject.top_up 1 }.to raise_error("Maximum limit of #{Oystercard::MAXIMUM_BALANCE} exceded")
+    end
+
+    it 'allows you to top up to the maxumum limit' do
+      expect(subject.top_up Oystercard::MAXIMUM_BALANCE).to eq subject.balance
+    end
+
   end
 
 end
